fa040ff4e539a15ca13dd23a108d9fb984991806
Stepan <steparesh@gmail.com>
2019-05-27 12:07:03 +0300

    test7

--- pipe.js
+++ pipe.js
@@ -30,7 +30,7 @@ const negate = x => x = -x;
 
 
 function pipe(...fns) {
-  return (args) => {
+  return (...args) => {
     fns.reduce((acc, fn) => {
       console.log(fn);
        return fn(acc);
@@ -40,14 +40,14 @@ function pipe(...fns) {
 
 
 
-console.log(pipe(multiply, addOne, square)([2, 3]));
+console.log(pipe(multiply, addOne, square)(2, 3));
 // console.log(pipe(multiply, addOne, square)(3, 4));
 //console.log(pipe(1, 2, 4, 5, 6, 7, 8, 9, 0));
 
-const test = (arg) => {
+const test = (...arg) => {
   console.log(...arg);
   console.log(arg);
   console.log(multiply(...arg));
 }
 
-test([3, 4])
+test(3, 4)
