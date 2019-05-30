e22d6ed3d825a4354b4266365792384ad2c4fc8c
Stepan Reshetnyak <sreshetnyak@waves.work>
2019-05-30 13:40:39 +0300

    whereFix1

--- test/where.test.ts
+++ test/where.test.ts
@@ -1,41 +1,26 @@
 
-export const lt = (a: number, b: number): boolean => a < b;
-export const gt = (a: number, b: number): boolean => a > b;
-export const equals = (a: any, b: any): boolean => a == b;
-
-
 function where(arg: Object) {
+    const ar: Array<boolean> = [];
     return (obj: Object) => {
-        for(let key in arg) {
-            return arg[key];
-        }
+        Object.keys(arg).forEach((el) => {
+            ar.push(arg[el](obj[el]));
+        });
+        console.log(ar.every(elem => elem == true) + '\n');
     }
 };
 
 const pred = where({
-   a: equals(obj[a], 'foo'),
-   x: gt(obj[x], 10),
-   y: lt(obj[y], 20)
+   a: (a: any, b = 'foo'): boolean => a == b, // equals
+   x: (a: number, b = 10): boolean => a > b, // gt
+   y: (a: number, b = 20): boolean => a < b //lt
 });
 
 pred({a: 'foo', x: 11, y: 19});
-
-// JS
-// const lt = (b) => (a) => a < b;
-// const gt = (b) => (a) => a > b;
-// const equals = (a, b) => a == b;
+pred({a: 'fo', x: 12, y: 19});
+pred({a: 'foo', x: 9, y: 19});
+pred({a: 'xxx', x: 11, y: 29});
 
 
-// function where(arg) {
-//     return (obj) => {
-//       for (let key in arg) {
-        
-//       }
-//     }
-// };
-
-// where({
-//    a: equals('foo'),
-//    x: gt(10),
-//    y: lt(20)
-// })({a: 'foo', x: 11, y: 19});
+//.every(elem => elem == true)
+//console.log(arg[el](obj[el]));
+  //          return arg[el](obj[el]);
