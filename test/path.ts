44eaa7a5bc89e119f23bc25b09c766431f1dd691
Stepan Reshetnyak <sreshetnyak@wavesplatform.com>
2019-07-01 16:14:25 +0300

    final version of pathjs

--- test/path.ts
+++ test/path.ts
@@ -5,13 +5,20 @@
 function path(arr: any, obj: any) {
     function search(obj: any, arr: any): any {
         let first = arr.shift();
-        if (obj.hasOwnProperty(first) && typeof(obj[first]) === 'object')
-            return search(obj[first], arr); 
-        else return obj[first];
+        if (obj[first] !== null) {
+            if (obj.hasOwnProperty(first) && typeof(obj[first]) === 'object' && arr.length !== 0)
+                return search(obj[first], arr); 
+            else return obj[first];
+        } else return undefined;
     }
-    return search(obj, arr)
+    return search(obj, arr);
 }
 
 console.log(path(['a', 'b'], {a: {b: 2}}));
 console.log(path(['a', 'b'], {c: {b: 2}}));
 console.log(path(['a', 'b', 'c'], {a: {b: {c: 3}}}));
+console.log(path(['a', 'b', 'c', 'd'], {a: {b: {c: {d: 5}}}}));
+console.log(path(['a', 'b'], {a: {b: {c: {d: 5}}}}));
+console.log(path(['a', 'b', 'c', 'd'], {a: {b: null}}));
+console.log(path(['a', 'b', 'c', 'd'], {a: {b: 7}}));
+
